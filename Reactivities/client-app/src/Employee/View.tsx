1feeece8ff1562bc71173a9671f9f717ec4b862c
Peeyushanand2 <peeyushanand2@gmail.com>
2024-04-14 19:42:59 +0530

    git ignore rm

---
+++ Reactivities/client-app/src/Employee/View.tsx
@@ -0,0 +1,18 @@
+export function ViewEmployeeDetails() {
+    return (
+        <>
+            <h3>this is the view employee page</h3>
+            <h3><City /></h3>
+        </>
+    );
+}
+
+export function City() {
+    return (
+        <>
+            <h3>My city is in bangalore</h3>
+        </>
+
+    );
+}
+
---
+++ Reactivities/client-app/src/Home.tsx
@@ -0,0 +1,9 @@
+export function LandingMain() {
+    return (
+        <>
+            <h1>Home landing page loaded...</h1>
+        </>
+
+    );
+
+}
