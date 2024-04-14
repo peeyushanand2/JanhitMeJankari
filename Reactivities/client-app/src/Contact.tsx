d8299728536eb48acdc6085214d50913c26fd722
Peeyushanand2 <peeyushanand2@gmail.com>
2024-04-14 16:40:13 +0530

    ReActivities solution added

---
+++ Reactivities/client-app/src/Contact.tsx
@@ -0,0 +1,7 @@
+export function Contact() {
+    return (
+        <>
+            <h2>Contact me on xyz@gmail.com</h2>
+        </>
+    );
+}
---
+++ Reactivities/client-app/src/Products.tsx
@@ -0,0 +1,15 @@
+export function ViewProducts() {
+    return (
+        <>
+            <ul className="navbar-nav">
+                <li className="nav-item">
+                    Apple
+                </li>
+                <li className="nav-item">
+                    Moto
+                </li>
+            </ul>
+        </>
+    );
+
+}
