4adccca2a0ca94cf2e2cfec0b8a47529ac4cefd5
peeyushanand2 <109307133+peeyushanand2@users.noreply.github.com>
2024-09-20 00:23:30 +0530

    added regions

--- Reactivities/InterviewPractice/Program.cs
+++ Reactivities/InterviewPractice/Program.cs
@@ -1,22 +1,40 @@
-﻿// Pyramid.InvertedHalfNumericPyramid();
-using InterviewPractice.PracticeQuestion.Integer;
+﻿using InterviewPractice.PracticeQuestion.Integer;
 using InterviewPractice.PracticeQuestion.Patterns;
 using InterviewPractice.PracticeQuestion.QuickTest;
 using InterviewPractice.PracticeQuestion.String;
 
+#region Array
+
+#endregion
+
+#region Integer
+//reverse integer
+int input = 123;
+int x = ReverseInteger.Reverse(input);
+Console.WriteLine($"Reverse of {input} is {x}");
+
+//reverse integer using Stack;
+int inputstack = -123;
+int xStack = ReverseInteger.ReverseUsingStack(inputstack);
+Console.WriteLine($"Reverse of {input} is {xStack}");
+#endregion
+
+#region Patterns
+// Pyramid.InvertedHalfNumericPyramid();
 
 Pyramid.NumericTranglePyramid();
+#endregion
 
-#region Without LSP
+#region C#Concepts
+//Without LSP
 Oranges orange = new Oranges();
 orange.Color();
 //even apple reference variable hoding the child object
 //and its vilation of LSPP
 Apples apples = new Oranges();
 apples.Color();
-#endregion
 
-#region with LSP
+// LSP
 //here fruilt base class object reference variable can be replaced with Orange class 
 //and Apple class
 //this is the LSP
@@ -24,14 +42,16 @@ Fruit obj = new Orange();
 obj.color();
 obj = new Apple();
 obj.color();
-#endregion
 
+//quick prograam
 ClildClass ch = new ClildClass();
 ch.Method();
 //how can we call base class method with child class object 
 BaseClass bs = new ClildClass();
 bs.Method();
+#endregion
 
+#region String
 //Count duplicate in string 
 CountDuplicateInString d = new CountDuplicateInString();
 d.CountDuplicate("Proograaam is test");
@@ -39,15 +59,7 @@ d.CountDuplicate("Proograaam is test");
 //Remove duplicate from string
 RemoveDuplicateFromString rem = new RemoveDuplicateFromString();
 rem.RemoveDuplicate("Proograaam is test");
+#endregion
 
-#region reverse integer;
-int input = 123;
-int x = ReverseInteger.Reverse(input);
-Console.WriteLine($"Reverse of {input} is {x}");
-#endregion
-#region reverse integer using Stack;
-int inputstack = -123;
-int xStack = ReverseInteger.ReverseUsingStack(inputstack);
-Console.WriteLine($"Reverse of {input} is {xStack}");
-#endregion
+
 Console.ReadKey();
