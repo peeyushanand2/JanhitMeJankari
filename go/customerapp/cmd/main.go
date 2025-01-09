0bf0bb7033c235c7fc34b04853a077b673695aff
peeyushanand2 <109307133+peeyushanand2@users.noreply.github.com>
2025-01-09 13:49:06 +0530

    api in go

--- go/customerapp/cmd/main.go
+++ go/customerapp/cmd/main.go
@@ -2,47 +2,73 @@ package main
 
 // Implement other methods for the type CustomerController
 import (
-	"customerapp/domain"
+	"customerapp/controller"
 	"customerapp/memstore"
+	"log"
+	"net/http"
 )
 
 // Inside the main function
 func main() {
-	controller := CustomerController{ // initialize customer controller
-		repository: memstore.NewCustomerRepository(),
-		//repository: mongodb.NewCustomerRepository(), // switching to another persistent store
+
+	repo := memstore.NewCustomerRepository() // With in-memory database
+
+	h := &controller.CustController{
+		Repository: repo, // Injecting dependency
 	}
-	var data []domain.Customer
-	data = make([]domain.Customer, 0, 4)
-	data = append(data, domain.Customer{
-		ID:    "cust101",
-		Name:  "Rahul",
-		Email: "rahul@gmail.com",
-	})
-	data = append(data, domain.Customer{
-		ID:    "cust102",
-		Name:  "Rahul2",
-		Email: "rahul2@gmail.com",
-	})
-	data = append(data, domain.Customer{
-		ID:    "cust103",
-		Name:  "Rahul3",
-		Email: "rahul3@gmail.com",
-	})
-	for _, v := range data {
-		controller.Add(v)
+	router := initializeRoutes(h) // configure routes
+	// CORS middleware
+	//router = cors.Default().Handler(router)
+	server := &http.Server{
+		Addr:    ":8080",
+		Handler: router,
 	}
-	newCustomer := domain.Customer{
-		ID:    "cust100",
-		Name:  "Rahulnew",
-		Email: "rahulnew@gmail.com",
-	}
-	controller.Update("cust103", newCustomer)
+	log.Println("Listening...")
+	server.ListenAndServe() // Run the http server
+	// controller := controller.CustController{ // initialize customer controller
+	// 	repository: memstore.NewCustomerRepository(),
+	// 	//repository: mongodb.NewCustomerRepository(), // switching to another persistent store
+	// }
+	// var data []domain.Customer
+	// data = make([]domain.Customer, 0, 4)
+	// data = append(data, domain.Customer{
+	// 	ID:    "cust101",
+	// 	Name:  "Rahul",
+	// 	Email: "rahul@gmail.com",
+	// })
+	// data = append(data, domain.Customer{
+	// 	ID:    "cust102",
+	// 	Name:  "Rahul2",
+	// 	Email: "rahul2@gmail.com",
+	// })
+	// data = append(data, domain.Customer{
+	// 	ID:    "cust103",
+	// 	Name:  "Rahul3",
+	// 	Email: "rahul3@gmail.com",
+	// })
+	// for _, v := range data {
+	// 	controller.Add(v)
+	// }
+	// newCustomer := domain.Customer{
+	// 	ID:    "cust100",
+	// 	Name:  "Rahulnew",
+	// 	Email: "rahulnew@gmail.com",
+	// }
+	// controller.Update("cust103", newCustomer)
 
-	controller.Delete("cust105")
+	// controller.Delete("cust105")
 
-	controller.FetchById("cust101")
+	// controller.FetchById("cust101")
 
-	controller.FetchAll()
+	// controller.FetchAll()
 
+}
+func initializeRoutes(h *controller.CustController) http.Handler {
+	mux := http.NewServeMux()
+	mux.HandleFunc("GET /api/customers", h.GetAllCustomers)
+	mux.HandleFunc("GET /api/customers/{id}", h.GetCustomerById)
+	mux.HandleFunc("POST /api/customers", h.CreateNewCustomer)
+	mux.HandleFunc("PUT /api/customers/{id}", h.UpdateCustomer)
+	mux.HandleFunc("DELETE /api/customers/{id}", h.DeleteCustomer)
+	return mux
 }
---
+++ go/customerapp/controller/customercontroller.go
@@ -0,0 +1,181 @@
+package controller
+
+import (
+	"customerapp/domain"
+	"encoding/json"
+	"errors"
+	"fmt"
+	"net/http"
+)
+
+// CustController Organises the CRUD operations at UI layer.
+type CustController struct {
+	Repository domain.CustomerRepository
+}
+
+//rest API //api/customers
+
+func (cc *CustController) GetAllCustomers(w http.ResponseWriter, r *http.Request) {
+	// Get all
+	if cust, err := cc.Repository.GetAll(); err != nil {
+		if errors.Is(err, domain.DataNotFound) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+
+	} else {
+		j, err := json.Marshal(cust)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.Header().Set("Content-Type", "application/json")
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	}
+}
+
+func (h *CustController) GetCustomerById(w http.ResponseWriter, r *http.Request) {
+	// Getting route parameter id
+	id := r.PathValue("id")
+	// Get by id
+	if note, err := h.Repository.GetById(id); err != nil {
+		if errors.Is(err, domain.DataNotFound) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	} else {
+		w.Header().Set("Content-Type", "application/json")
+		j, err := json.Marshal(note)
+		if err != nil {
+			http.Error(w, err.Error(), http.StatusInternalServerError)
+		}
+		w.WriteHeader(http.StatusOK)
+		w.Write(j)
+	}
+}
+
+func (cc *CustController) CreateNewCustomer(w http.ResponseWriter, r *http.Request) {
+	var cust domain.Customer
+	//get incomming request
+	err := json.NewDecoder(r.Body).Decode(&cust)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	//Success
+	if err := cc.Repository.Create(cust); err != nil {
+		if errors.Is(err, domain.DataExist) {
+			http.Error(w, err.Error(), http.StatusBadRequest)
+			return
+		}
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+	message := fmt.Sprintf("Customer created successfully: %s", cust.ID)
+	j, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusCreated)
+	w.Write(j)
+}
+
+func (cust *CustController) UpdateCustomer(w http.ResponseWriter, r *http.Request) {
+	// Getting route parameter id
+	id := r.PathValue("id")
+	var note domain.Customer
+	// Decode the incoming note json
+	err := json.NewDecoder(r.Body).Decode(&note)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	// Update
+	if err := cust.Repository.Update(id, note); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	w.WriteHeader(http.StatusNoContent)
+}
+
+func (h *CustController) DeleteCustomer(w http.ResponseWriter, r *http.Request) {
+	// Getting route parameter id
+	id := r.PathValue("id")
+	// delete
+	if err := h.Repository.Delete(id); err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+	message := fmt.Sprintf("Customer deleted: %s", id)
+	j, err := json.Marshal(message)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+	}
+	w.WriteHeader(http.StatusNoContent)
+	w.Write(j)
+}
+
+// Add function to add new customer.
+func (cc CustController) Add(c domain.Customer) {
+	fmt.Println("Add operation")
+	err := cc.Repository.Create(c)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("new Customer has been created")
+	fmt.Println("-----------------------")
+}
+
+func (cc CustController) Update(id string, c domain.Customer) {
+	fmt.Println("Update operation")
+	err := cc.Repository.Update(id, c)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("new Customer has been updated")
+	fmt.Println("-----------------------")
+}
+
+func (cc CustController) Delete(id string) {
+	fmt.Println("Delete operation")
+	err := cc.Repository.Delete(id)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	fmt.Println("new customer has been deleted")
+	fmt.Println("-----------------------")
+}
+func (cc CustController) FetchById(id string) {
+	fmt.Println("FetchById operation")
+	data, err := cc.Repository.GetById(id)
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+
+	fmt.Println("ID:", data.ID)
+	fmt.Println("Name:", data.Name)
+	fmt.Println("ID:", data.Email)
+	fmt.Println("-----------------------")
+}
+func (cc CustController) FetchAll() {
+	fmt.Println("FetchAll operation")
+	data, err := cc.Repository.GetAll()
+	if err != nil {
+		fmt.Println("Error: ", err)
+		return
+	}
+	for _, v := range data {
+		fmt.Println(v.ID)
+		fmt.Println(v.Name)
+		fmt.Println(v.Email)
+		fmt.Println("-----------------------")
+	}
+}
